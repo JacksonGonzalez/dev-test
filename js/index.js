7bcc051093ed7ac565eb1a37f51397a6ffa1e8cf
JacksonGonzalez <jaesgoca@gmail.com>
2018-12-08 14:51:15 -0500

    ya se consumio el ENDPOINT y se remplazo las img y los textos

--- js/index.js
+++ js/index.js
@@ -2,6 +2,24 @@ $(document).ready(function(){
     // PRUEBA CUANDO CARGUE
     // alert('Hola');
 
+    // HACER EL LLAMADO A LA API
+    fetch('https://api.myjson.com/bins/17ocpi')
+    .then((respuesta) => {
+        return respuesta.json();
+    }).then((respuesta) => {
+        $("#img-1").attr('src', respuesta.slide_1.image);
+        $("#t-1").text(respuesta.slide_1.title);
+        $("#p-1").text(respuesta.slide_1.text);
+
+        $("#img-2").attr('src', respuesta.slide_2.image);
+        $("#t-2").text(respuesta.slide_2.title);
+        $("#p-2").text(respuesta.slide_2.text);
+
+        $("#img-3").attr('src', respuesta.slide_3.image);
+        $("#t-3").text(respuesta.slide_3.title);
+        $("#p-3").text(respuesta.slide_3.text);
+    });
+
     // REVISAR QUE NO HAYAN CAMBIOS EN LAS OPCIONES
     function verificarConsola(){
         let q1 = $("#q1").val();
